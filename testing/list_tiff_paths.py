5307b3a0c1eabb5d50944188ff27cec4e9a3f843
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-04 17:04:17 +0200

    ..

--- testing/list_tiff_paths.py
+++ testing/list_tiff_paths.py
@@ -14,6 +14,7 @@ for tomo in os.listdir(main_folder + sample):
         if f[-4:] == 'tiff':
             file_list.append(main_folder + sample + tomo + '/' + subfolder + f)
             break
+    print(tomo)
 
 print(c)
 print(len(file_list))
