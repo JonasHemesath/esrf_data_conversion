3747a517cdf0443451a51f1f104b862f5a9ab2a7
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-04 17:03:12 +0200

    ..

--- testing/list_tiff_paths.py
+++ testing/list_tiff_paths.py
@@ -1,9 +1,19 @@
 import os
+import json
 
 main_folder = '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/'
 sample = 'zf11_hr/'
 subfolder = 'recs_2024_04/'
 
+file_list = []
+c = 0
+
 for tomo in os.listdir(main_folder + sample):
+    c += 1
     for f in os.listdir(main_folder + sample + tomo + '/' + subfolder):
-        print(f[-4:])
+        if f[-4:] == 'tiff':
+            file_list.append(main_folder + sample + tomo + '/' + subfolder + f)
+            break
+
+print(c)
+print(len(file_list))
