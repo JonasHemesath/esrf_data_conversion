37094d73d66a0872615a466a525e997812e41286
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-08 13:29:58 +0200

    ..

--- testing/plot_percentiles.py
+++ testing/plot_percentiles.py
@@ -4,9 +4,9 @@ import json
 with open('percentiles_esrf_data.json', 'r') as f:
     d = json.load(f)
     
-    plt.hist(d['zf13_hr2']['0.39% percentile'])
+    plt.hist(d['zf13_hr2']['0.39% percentile'], bins=100)
     print('0.39% percentile', min(d['zf13_hr2']['0.39% percentile']))
     plt.savefig('zf13_hr2_0_39% percentile.png')
-    plt.hist(d['zf13_hr2']['99.61% percentile'])
+    plt.hist(d['zf13_hr2']['99.61% percentile'], bins=100)
     print('99.61% percentile', max(d['zf13_hr2']['99.61% percentile']))
     plt.savefig('zf13_hr2_99_61% percentile.png')
