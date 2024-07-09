d44809f8532deba6ee1034a0f377dd93e91cb4e2
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-09 08:51:48 +0200

    ..

--- testing/plot_percentiles.py
+++ testing/plot_percentiles.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import json
 
-with open('percentiles_esrf_data.json', 'r') as f:
+with open('percentiles_esrf_data_zf11.json', 'r') as f:
     d = json.load(f)
     
-    plt.hist(d['zf13_hr2']['0.39% percentile'], bins=100)
-    print('0.39% percentile', min(d['zf13_hr2']['0.39% percentile']))
-    plt.savefig('zf13_hr2_0_39% percentile.png')
-    plt.hist(d['zf13_hr2']['99.61% percentile'], bins=100)
-    print('99.61% percentile', max(d['zf13_hr2']['99.61% percentile']))
-    plt.savefig('zf13_hr2_99_61% percentile.png')
+    plt.hist(d['zf11_hr']['0.39% percentile'], bins=100)
+    print('0.39% percentile', min(d['zf11_hr']['0.39% percentile']))
+    plt.savefig('zf11_hr_0_39% percentile.png')
+    plt.hist(d['zf11_hr2']['99.61% percentile'], bins=100)
+    print('99.61% percentile', max(d['zf11_hr']['99.61% percentile']))
+    plt.savefig('zf11_hr_99_61% percentile.png')
