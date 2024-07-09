754ccb808ffb73a9cad11e046302cb5a53144006
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-09 09:15:24 +0200

    ..

--- testing/list_tiffs_with_size.py
+++ testing/list_tiffs_with_size.py
@@ -2,12 +2,12 @@ import os
 import json
 
 main_folder = '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/'
-sample = 'zf11_hr/'
+sample = 'zf13_hr2/'
 subfolder = 'recs_2024_04/'
 
 
 
-with open('tiff_files_zf11_hr.txt', 'w') as file:
+with open('tiff_files_zf13_hr2.txt', 'w') as file:
     for tomo in os.listdir(main_folder + sample):
         
         file.write(tomo + '\n')
