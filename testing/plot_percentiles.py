b47fb1b4725be7c99486f5e99c7e205f4e94ae14
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-09 08:52:34 +0200

    ..

--- testing/plot_percentiles.py
+++ testing/plot_percentiles.py
@@ -7,6 +7,6 @@ with open('percentiles_esrf_data_zf11.json', 'r') as f:
     plt.hist(d['zf11_hr']['0.39% percentile'], bins=100)
     print('0.39% percentile', min(d['zf11_hr']['0.39% percentile']))
     plt.savefig('zf11_hr_0_39% percentile.png')
-    plt.hist(d['zf11_hr2']['99.61% percentile'], bins=100)
+    plt.hist(d['zf11_hr']['99.61% percentile'], bins=100)
     print('99.61% percentile', max(d['zf11_hr']['99.61% percentile']))
     plt.savefig('zf11_hr_99_61% percentile.png')
