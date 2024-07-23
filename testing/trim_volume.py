7d555306f8c8573fd7bc4bc4111d3dbf15426c81
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-23 14:22:50 +0200

    ..

---
+++ testing/trim_volume.py
@@ -0,0 +1,24 @@
+import imagej
+import scyjava
+
+
+import numpy as np
+
+load_path = 'cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff'
+
+
+scyjava.config.add_option('-Xmx500g')
+ij = imagej.init()
+print('ij loaded')
+
+
+im = ij.io().open(load_path)
+
+x = 624
+dx = 3600
+y = 480
+dy = 3936
+z = 0
+dz = 1800
+print(im.shape)
+#im_trim = im[]
