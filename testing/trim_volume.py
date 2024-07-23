fa9feb5b7082f42b4781e23a213761126ffb1952
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-23 14:44:12 +0200

    ..

--- testing/trim_volume.py
+++ testing/trim_volume.py
@@ -5,7 +5,7 @@ import scyjava
 import numpy as np
 
 load_path = 'cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff'
-
+save_path = 'cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr_example_data/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff'
 
 scyjava.config.add_option('-Xmx500g')
 ij = imagej.init()
@@ -21,4 +21,9 @@ dy = 3936
 z = 0
 dz = 1800
 print(im.shape)
+
+im_trim = im[x:x+dx, y:y+dy, z:z+dz]
+
+ij.io().save(im_trim, save_path)
+
 #im_trim = im[]
