1fe90cf8fc8d3083b70a6bd9a3f48cf59eb108c5
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-26 10:09:31 +0200

    ..

--- testing/downsampling.py
+++ testing/downsampling.py
@@ -7,7 +7,7 @@ import skimage
 
 file = '/cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr_example_data/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff'
 
-save_path = '/cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr_example_data/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff''/cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr_example_data_2x_downsample/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff'
+save_path = '/cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr_example_data_2x_downsample/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff'
 
 scyjava.config.add_option('-Xmx500g')
 ij = imagej.init()
