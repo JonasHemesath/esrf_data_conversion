902dec662f5b4a50e5a80cf2192fa0fa98826ae9
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-26 16:52:38 +0200

    ..

--- testing/show_tiff.py
+++ testing/show_tiff.py
@@ -1,7 +1,7 @@
 import tifffile
 import matplotlib.pyplot as plt
 
-path = '/home/jonas/mnt/cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr_example_data/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff'
+path = '/home/jonas/mnt/cajal/scratch/projects/xray/bm05/converted_data/zf11/zf11_hr_x08600_y-81680_z-958120__1_1_0000pag_db0100.tiff'
 
 im = tifffile.imread(path, key=1799)
 
