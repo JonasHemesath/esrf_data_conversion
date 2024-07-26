9c7999bcf8f715b3cf7eac26c093e998cc41de31
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-26 13:04:30 +0200

    ..

--- testing/downsampling.py
+++ testing/downsampling.py
@@ -3,6 +3,7 @@ import scyjava
 
 import tifffile
 import skimage
+import numpy as np
 
 file = '/cajal/scratch/projects/xray/bm05/converted_data/zf14_s1_hr_example_data/zf14_s1_hr_x03880_y04180_z-898700_1_1_0000pag_db0020.tiff'
 
@@ -16,8 +17,8 @@ print('ij loaded')
 im = ij.py.to_xarray(ij.io().open(file))
 print('im load', im.shape)
 print(im.dtype)
-"""
-im_d = skimage.transform.resize(im, (im.shape[0]//4, im.shape[1]//4, im.shape[2]//4), anti_aliasing=True)
+
+im_d = skimage.transform.resize(im, (im.shape[0]//4, im.shape[1]//4, im.shape[2]//4), anti_aliasing=True).astype(np.uint8)
 print('im transformed', im_d.shape, type(im_d))
 
 
@@ -27,4 +28,4 @@ for i in range(im_d.shape[0]):
     while len(idx) < 4:
         idx = '0' + idx
     tifffile.imwrite(save_path + idx + '.tiff', im_d[i,:,:])
-print('ImageJ: image saved')"""
+print('ImageJ: image saved')
