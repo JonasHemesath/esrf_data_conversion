b5a956901650ef115f6e2494aeb2e22910aa9f62
Jonas Hemesath <jonas-hemesath@web.de>
2024-07-26 13:58:24 +0200

    ..

--- testing/downsampling.py
+++ testing/downsampling.py
@@ -18,7 +18,7 @@ im = ij.py.to_xarray(ij.io().open(file))
 print('im load', im.shape)
 print(im.dtype)
 
-im_d = skimage.transform.resize(im[0:16,:,:], (im.shape[0]//4, im.shape[1]//4, im.shape[2]//4), anti_aliasing=True)
+im_d = skimage.transform.resize(im, (im.shape[0]//4, im.shape[1]//4, im.shape[2]//4), anti_aliasing=True)
 print('im transformed', im_d.shape, type(im_d))
 print(im_d.dtype)
 print(np.min(im_d))
