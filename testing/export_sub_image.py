7ed16f28296c10e38366ca2d387bcb0620993572
Jonas Hemesath <jonas-hemesath@web.de>
2024-09-11 13:40:07 +0200

    ..

--- testing/export_sub_image.py
+++ testing/export_sub_image.py
@@ -1,6 +1,6 @@
 import tifffile
 
-path = '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/zf11_hr/recs_2024_04/zf11_hr_x08600_y-35120_z-892120__1_1_0000pag_db0100.tiff'
+path = '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/zf11_hr/zf11_hr_x0.8600_y-3.5120_z-89.2120_/recs_2024_04/zf11_hr_x08600_y-35120_z-892120__1_1_0000pag_db0100.tiff'
 
 im = tifffile.imread(path, key=10)
 
