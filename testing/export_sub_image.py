ea120936753c1b549c4c345ca2ca6affc03d6a90
Jonas Hemesath <jonas-hemesath@web.de>
2024-09-11 13:41:09 +0200

    ..

--- testing/export_sub_image.py
+++ testing/export_sub_image.py
@@ -4,4 +4,4 @@ path = '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/zf11_hr/zf11_h
 
 im = tifffile.imread(path, key=10)
 
-tifffile.imwrite('/cajal/scratch/projects/xray/bm05/converted_data/fourier_filter_test/recs_2024_04/zf11_hr_x08600_y-35120_z-892120__1_1_0000pag_db0100_10.tiff')
+tifffile.imwrite('/cajal/scratch/projects/xray/bm05/converted_data/fourier_filter_test/recs_2024_04/zf11_hr_x08600_y-35120_z-892120__1_1_0000pag_db0100_10.tiff', im)
