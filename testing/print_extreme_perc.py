74cbc6722ce45f947568c727e585ec8b6a3fcc67
Jonas Hemesath <jonas-hemesath@web.de>
2024-09-24 13:19:56 +0200

    ..

--- testing/print_extreme_perc.py
+++ testing/print_extreme_perc.py
@@ -1,6 +1,8 @@
 import json
 
 
+r = 30
+
 sample = 'zf11_hr'
 
 with open('percentiles_esrf_data_' + sample + '.json', 'r') as f:
@@ -8,13 +10,13 @@ with open('percentiles_esrf_data_' + sample + '.json', 'r') as f:
 
 list1, list2 = zip(*sorted(zip(data['0.39% percentile'], data['file'])))
 print('low 0.39% percentile')
-for i in range(0,5):
+for i in range(0,r):
     print(list2[i], list1[i])
 
 list1, list2 = zip(*sorted(zip(data['99.61% percentile'], data['file'])))
 print('high 99.61% percentile')
 n = len(list1)
-for i in range(n-5,n):
+for i in range(n-r,n):
     print(list2[i], list1[i])
 
 
