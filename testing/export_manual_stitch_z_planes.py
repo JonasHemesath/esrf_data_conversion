043a3ce1ca53cb6d7f79cc289b79be9ce581b6a8
Jonas Hemesath <jonas-hemesath@web.de>
2025-01-16 16:30:47 +0100

    xz yz stitch

---
+++ testing/export_manual_stitch_z_planes.py
@@ -0,0 +1,41 @@
+import os
+import tifffile
+import skimage
+
+load_path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf11_hr/'
+
+save_path = '/cajal/scratch/projects/xray/bm05/converted_data/zf11_manual_stitch/'
+
+filter1 = 'zf11'
+filter2 = 'tiff'
+
+z = 1990
+
+c = 1
+
+for f in os.listdir(load_path):
+    if f[0:4] == filter1 and f[-4:] == filter2:
+        print(c)
+        c += 1
+        
+        im = tifffile.imread(load_path + f, key=range(0,z))
+        for i in range(2):
+            if i:
+                im_d = skimage.transform.resize(im[:,im.shape[1]//2,:], (im.shape[0]//2, im.shape[2]//2), anti_aliasing=True)
+                name = 'xz'
+                part_id = 'y'
+            else:
+                im_d = skimage.transform.resize(im[im.shape[0]//2,:,:], (im.shape[1]//2, im.shape[2]//2), anti_aliasing=True)
+                name = 'yz'
+                part_id = 'x'
+            parts = f.split('_')
+            for part in parts:
+                if part[0] == part_id and part[1] != 'f':
+                    pos = part
+                    break
+            
+            subfolder = pos + '_' + name + '/'
+
+            if not os.path.isdir(save_path + subfolder):
+                os.makedirs(save_path + subfolder)
+            tifffile.imwrite(save_path + subfolder + f, im_d)
