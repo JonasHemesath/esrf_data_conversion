e45e613cddb5a8e4a296d1f3dce0e47033492c49
Jonas Hemesath <jonas-hemesath@web.de>
2025-01-24 09:32:23 +0100

    get_subvolume.py

---
+++ testing/get_subvolume.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf11_hr/stitched_1055.0_3198.0_1813.0_20384x18036x12857.raw'
+
+
+data = np.memmap(path, np.uint8, 'r', 0, (512,512,512))
+
+print(data[0,:,:])
+print(data[:,0,:])
+print(data.shape)
