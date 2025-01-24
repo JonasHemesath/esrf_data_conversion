6b6aaf6a3898c0580dfbfdafd3ec32edb779f675
Jonas Hemesath <jonas-hemesath@web.de>
2025-01-24 14:23:36 +0100

    ..

--- testing/get_subvolume.py
+++ testing/get_subvolume.py
@@ -9,8 +9,10 @@ pi = Pi2()
 path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf11_hr/stitched_1055.0_3198.0_1813.0_20384x18036x12857.raw'
 
 
-data = np.memmap(path, np.int8, 'r', 2160000000000, (512,512,512), order='C')
+#data = np.memmap(path, np.int8, 'r', 2160000000000, (512,512,512), order='C')
 
+data = pi.readrawblock('img', path, 5000, 5000, 5000, 512,512,512)
+print(type(data))
 print(data[0,:,:])
 print(data[:,0,:])
 print(data.shape)
