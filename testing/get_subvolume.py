5746eb695b2da9f5cb99e72fc8fb19343a445dc6
Jonas Hemesath <jonas-hemesath@web.de>
2025-01-24 09:57:34 +0100

    ..

--- testing/get_subvolume.py
+++ testing/get_subvolume.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+#import sys
+#sys.path.append("/cajal/nvmescratch/users/johem/pi2_new/pi2/bin-linux64/release-nocl/")
+#from pi2py2 import *
+
+#pi = Pi2()
 
 path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf11_hr/stitched_1055.0_3198.0_1813.0_20384x18036x12857.raw'
 
 
-data = np.memmap(path, np.uint8, 'r', 2160000000000, (512,512,512))
+data = np.memmap(path, np.uint8, 'r', 2160000000000, (512,512,512), order='F')
 
 print(data[0,:,:])
 print(data[:,0,:])
