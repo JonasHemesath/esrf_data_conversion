1172bd6ec71959d6c7738b8bc484adc313c159dc
Jonas Hemesath <jonas-hemesath@web.de>
2025-01-24 15:30:48 +0100

    ..

--- testing/get_subvolume.py
+++ testing/get_subvolume.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf11_hr/stitched_1055.0_3198.0_1813.0_20384x18036x12857.raw'
 
 
-data = np.memmap(path, dtype=np.uint8, mode='r', shape=(20384,18036,12857), order='C')
+data = np.memmap(path, dtype=np.uint8, mode='r', shape=(20384,18036,12857), order='F')
 
 #data = pi.readrawblock('img', path, 5000, 5000, 5000, 512,512,512, 'unit8', 20384,18036,12857)
 #print(data.get_data_type())
