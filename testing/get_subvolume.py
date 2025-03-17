574e7459a6246d35395286634d49705e8c94e7c1
Jonas Hemesath <jonas-hemesath@web.de>
2025-03-17 09:14:58 +0100

    new zf11 vol

--- testing/get_subvolume.py
+++ testing/get_subvolume.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 
 #pi = Pi2()
 
-path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf11_hr/stitched_1055.0_3198.0_1813.0_20384x18036x12857.raw'
+path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf11_hr/stitched_1048.0_3227.0_1813.0_20389x18041x12876.raw'
 
 
-data = np.memmap(path, dtype=np.uint8, mode='r', shape=(20384,18036,12857), order='F')
+data = np.memmap(path, dtype=np.uint8, mode='r', shape=(20389,18041,12876), order='F')
 
 #data = pi.readrawblock('img', path, 5000, 5000, 5000, 512,512,512, 'unit8', 20384,18036,12857)
 #print(data.get_data_type())
