17e9c9b58f4ff32c50d45c087a28d11a480ec4f3
Jonas Hemesath <jonas-hemesath@web.de>
2025-03-17 10:23:50 +0100

    ..

--- testing/export_test_volumes.py
+++ testing/export_test_volumes.py
@@ -25,9 +25,9 @@ postions_resin = [(6150, 10150, 7750), (6300, 3350, 7750), (11050, 6725, 7750), 
 for p in postions_tissue:
     v = data[p[0]:p[0]+size, p[1]:p[1]+size, p[2]:p[2]+size]
     name = 'tissue' + str(p[0]) + '_' + str(p[1]) + '_' + str(p[2]) + '.npy'
-    np.save(save_path+'tissue/'+name)
+    np.save(save_path+'tissue/'+name, v)
 
 for p in postions_resin:
     v = data[p[0]:p[0]+size, p[1]:p[1]+size, p[2]:p[2]+size]
     name = 'resin' + str(p[0]) + '_' + str(p[1]) + '_' + str(p[2]) + '.npy'
-    np.save(save_path+'resin/'+name)
+    np.save(save_path+'resin/'+name, v)
