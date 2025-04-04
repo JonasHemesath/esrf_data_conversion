386515164bd8dfc1b2c44e083aaeaa7acc39edbc
hemesath <jonas-hemesath@web.de>
2025-04-04 14:28:32 +0200

    ..

--- denoising/get_stitched_overlapping_volumes.py
+++ denoising/get_stitched_overlapping_volumes.py
@@ -58,7 +58,9 @@ if dim2 == dim1:
         for y in range(math.floor(dim1[1]/cube_size)):
             for z in range(math.floor(dim1[2]/cube_size)):
                 if np.sum(overlap_mask[x:x+cube_size, y:y+cube_size, z:z+cube_size]) == cube_size**3:
+                    print('Writing:', f'{count}_split1.tiff')
                     tifffile.imwrite(f'{count}_split1.tiff', data=vol1[x:x+cube_size, y:y+cube_size, z:z+cube_size], imagej=True)
+                    print('Writing:', f'{count}_split2.tiff')
                     tifffile.imwrite(f'{count}_split2.tiff', data=vol2[x:x+cube_size, y:y+cube_size, z:z+cube_size], imagej=True)
                     count += 1
 
