82b1c63362105901206adae147f26618a8a1aeef
hemesath <jonas-hemesath@web.de>
2025-04-04 13:49:45 +0200

    ..

--- denoising/get_stitched_overlapping_volumes.py
+++ denoising/get_stitched_overlapping_volumes.py
@@ -1,9 +1,51 @@
+import os
 import subprocess
 import numpy as np
+
+
+def get_two_largest_raw_files():
+    """
+    Iterates over the current working directory (the directory in which this script is run)
+    and returns the file names of the two largest files with a '.raw' extension.
+
+    Returns:
+        list: A list containing the filenames of the two largest '.raw' files.
+              If fewer than two '.raw' files are found, returns a list with what is available.
+    """
+    cwd = os.getcwd()  # Current working directory
+    raw_files = []
+
+    # Iterate over the files in the current directory.
+    for entry in os.scandir(cwd):
+        if entry.is_file() and entry.name.lower().endswith('.raw'):
+            try:
+                size = entry.stat().st_size
+                raw_files.append((entry.name, size))
+            except OSError:
+                # Skip files that cause errors when attempting to access stat info.
+                continue
+
+    # Sort the list of files by their size in descending order.
+    raw_files.sort(key=lambda x: x[1], reverse=True)
+
+    # Return only the filenames of the two largest files.
+    return [file[0] for file in raw_files[:2]]
 
 
 p = subprocess.Popen(['python', '/cajal/nvmescratch/users/johem/pi2_new/pi2/bin-linux64/release-nocl/nr_stitcher_jh.py', 'stitch_settings.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 p.communicate()
 
+raw_files = get_two_largest_raw_files()
 
+dim1 = (int(c) for c in raw_files[0].split('_')[-1].split('.')[0].split('x'))
+
+vol1 = np.memmap(raw_files[0], dtype=np.uint8, mode='r', shape=dim1, order='F')
+
+dim2 = (int(c) for c in raw_files[1].split('_')[-1].split('.')[0].split('x'))
+
+vol2 = np.memmap(raw_files[1], dtype=np.uint8, mode='r', shape=dim1, order='F')
+print(dim1)
+print(raw_files[0])
+print(dim2)
+print(raw_files[1])
--- testing/transform_to_png.py
+++ testing/transform_to_png.py
@@ -1,29 +1,29 @@
 import os
 import tifffile
-from PIL import Image
 import numpy as np
 import cv2
 
-load_path = 'J:/Jonas Hemesath/EM/pw_samples_reimaged/pw0001/'
+load_path = 'C:/Users/hemesath/Desktop/zf13_hr2_stitching/zf13_hr2_manual_stitch/'
 
-save_path = 'J:/Jonas Hemesath/EM/pw_samples_reimaged/test/'
+save_path = 'C:/Users/hemesath/Desktop/zf13_hr2_stitching/pngs/'
 
 c = 1
 for folder in os.listdir(load_path):
+    
     for image in os.listdir(load_path + folder):
-        if image[-4:] == '.tif':
+        if image[-4:] == 'tiff':
             print(image)
             print(c)
             c+=1
             im = tifffile.imread(load_path + folder + '/' + image)
             #print(im)
 
-            im[200:250,:] = 0
+            #im[200:250,:] = 0
 
             im_4 = np.zeros((im.shape[0], im.shape[1], 4))
 
             for i in range(3):
-                im_4[:,:,i] = im
+                im_4[:,:,i] = im * 255
 
             im_4[:,:,3][im == 0] = 0
             im_4[:,:,3][im > 0] = 127
@@ -32,5 +32,5 @@ for folder in os.listdir(load_path):
 
             if not os.path.isdir(save_path + folder):
                 os.makedirs(save_path + folder)
-            cv2.imwrite(save_path + folder + '/' + image[0:-3] + 'png', im_4)
+            cv2.imwrite(save_path + folder + '/' + image[0:-4] + 'png', im_4)
 
