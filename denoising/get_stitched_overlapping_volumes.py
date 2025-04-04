588b7a9e4d12c0cd284dca73bd3ce924b9008cec
hemesath <jonas-hemesath@web.de>
2025-04-04 16:16:30 +0200

    ..

--- denoising/get_stitched_overlapping_volumes.py
+++ denoising/get_stitched_overlapping_volumes.py
@@ -3,7 +3,25 @@ import subprocess
 import numpy as np
 import math
 import tifffile
+from scipy.ndimage import binary_closing
 
+def fill_holes_in_mask(mask, structure=None):
+    """
+    Applies a binary closing to fill in small holes in the binary mask.
+    
+    Parameters:
+        mask : np.ndarray
+            A 3D boolean array where True indicates a voxel is considered valid.
+        structure : np.ndarray or None
+            The structuring element used for the closing operation.
+            If None, a 3x3x3 cube is used.
+            
+    Returns:
+        np.ndarray: The processed mask with holes filled.
+    """
+    if structure is None:
+        structure = np.ones((3, 3, 3), dtype=bool)
+    return binary_closing(mask, structure=structure)
 
 def get_two_largest_raw_files():
     """
@@ -54,8 +72,12 @@ if dim2 == dim1:
     vol2 = np.memmap(raw_files[1], dtype=np.uint8, mode='r', shape=dim1, order='F')
 
     overlap_mask = (vol1 > 0) & (vol2 > 0)
+    filled_mask = fill_holes_in_mask(overlap_mask)
+    diff_mask = ((filled_mask.astype(np.uint8).T+1)-(overlap_mask.astype(np.uint8).T+1))*122
     print('number of overlapping voxel:', np.sum(overlap_mask))
     tifffile.imwrite('overlap_mask.tiff', overlap_mask.astype(np.uint8).T*255, imagej=True)
+    tifffile.imwrite('overlap_mask.tiff', filled_mask.astype(np.uint8).T*255, imagej=True)
+    tifffile.imwrite('subtract_mask.tiff', diff_mask, imagej=True)
     overlap_list = []
     count = 0
     for x in range(math.floor(dim1[0]/cube_size)):
