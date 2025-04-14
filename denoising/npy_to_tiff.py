d948d922588d19ade12674e09b2a2532a86e8fe8
hemesath <jonas-hemesath@web.de>
2025-04-14 09:15:24 +0200

    ..

--- denoising/npy_to_tiff.py
+++ denoising/npy_to_tiff.py
@@ -5,8 +5,9 @@ import numpy as np
 for file in os.listdir():
     if file[-3:] == 'npy':
         data = np.load(file)
-        os.makedirs(file[0:-4])
-        for i in data.shape[0]:
+        if not os.path.isdir(file[0:-4]):
+            os.makedirs(file[0:-4])
+        for i in range(data.shape[0]):
             name = str(i)
             while len(name) < 5:
                 name = '0' + name
