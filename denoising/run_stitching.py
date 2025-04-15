ff7d287983ba4c0d5b7f6acba727feb1eca26a22
hemesath <jonas-hemesath@web.de>
2025-04-15 16:46:21 +0200

    ..

--- denoising/run_stitching.py
+++ denoising/run_stitching.py
@@ -5,8 +5,9 @@ parent_folder = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/z
 
 for folder in os.listdir(parent_folder):
     if os.path.isdir(parent_folder + folder):
+        wd = parent_folder + folder
 
-        p = subprocess.Popen(['python', '/cajal/nvmescratch/users/johem/pi2_new/pi2/bin-linux64/release-nocl/nr_stitcher_jh.py', 'stitch_settings.txt'],
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        p.communicate()
+        subprocess.Popen(['python', '/cajal/nvmescratch/users/johem/pi2_new/pi2/bin-linux64/release-nocl/nr_stitcher_jh.py', 'stitch_settings.txt'],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd)
+        
 
