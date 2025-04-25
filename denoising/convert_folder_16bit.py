5062eb2ca801d0cba201040c4e0b1ed6f6fd29cf
hemesath <jonas-hemesath@web.de>
2025-04-25 16:07:44 +0200

    ..

--- denoising/convert_folder_16bit.py
+++ denoising/convert_folder_16bit.py
@@ -7,10 +7,11 @@ parent_folder = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/z
 processes = []
 
 for folder in os.listdir(parent_folder):
-    wd = parent_folder + folder
-    processes.append(subprocess.Popen(['srun', '--time=7-0', '--gres=gpu:0', '--mem=900000', '--tasks', '1', '--cpus-per_task', '32', '--pty', 'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/denoising/convert_16bit.py'],
-                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd))
-    print('Subprocess started in', folder)
+    if os.path.isdir(parent_folder + folder):
+        wd = parent_folder + folder
+        processes.append(subprocess.Popen(['srun', '--time=7-0', '--gres=gpu:0', '--mem=900000', '--tasks', '1', '--cpus-per_task', '32', '--pty', 'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/denoising/convert_16bit.py'],
+                                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd))
+        print('Subprocess started in', folder)
 
 output_str = 'Process outputs:\n\n'
 for i,p in enumerate(processes):
