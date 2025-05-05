69a1d176750efb0e543759b5ec2e91afbb967fc2
hemesath <jonas-hemesath@web.de>
2025-05-05 14:55:57 +0200

    added test pi2 for high bit depth

---
+++ denoising/test_pi2.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("/cajal/nvmescratch/users/johem/pi2_new/pi2/bin-linux64/release-nocl/")
+
+from pi2py2 import *
+pi = Pi2()
+
+
+img = pi.read("/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf13_denoising_GT/37/zf13_hr2_x12130_y-07600_z-958520__1_1_0000pag_db0100_16bit.tiff")
+pi.writeraw(img, "output")
