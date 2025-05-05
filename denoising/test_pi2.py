0b480df8c4bfa55062513501955965d08d8a10d2
hemesath <jonas-hemesath@web.de>
2025-05-05 15:54:59 +0200

    ..

--- denoising/test_pi2.py
+++ denoising/test_pi2.py
@@ -5,7 +5,7 @@ from pi2py2 import *
 pi = Pi2()
 
 print("start reading image")
-img = pi.read("/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf13_denoising_GT/37/zf13_hr2_x12130_y-07600_z-958520__1_1_0000pag_db0100.tiff")
+img = pi.read("/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf13_denoising_GT/36/zf13_hr2_x-11150_y04160_z-945320__1_1_0000pag_db0100.tiff")
 print("reading image done\n")
 print("start writing image")
 pi.writeraw(img, "output")
