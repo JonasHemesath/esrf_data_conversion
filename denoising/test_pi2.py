28bff6b100bb2a468b082c19e2b7e54040cb8f85
hemesath <jonas-hemesath@web.de>
2025-05-05 14:59:41 +0200

    ..

--- denoising/test_pi2.py
+++ denoising/test_pi2.py
@@ -4,6 +4,9 @@ sys.path.append("/cajal/nvmescratch/users/johem/pi2_new/pi2/bin-linux64/release-
 from pi2py2 import *
 pi = Pi2()
 
-
+print("start reading image")
 img = pi.read("/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf13_denoising_GT/37/zf13_hr2_x12130_y-07600_z-958520__1_1_0000pag_db0100_16bit.tiff")
+print("reading image done\n")
+print("start writing image")
 pi.writeraw(img, "output")
+print("writing image done\n")
