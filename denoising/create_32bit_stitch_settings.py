86146404033404427c934ec39cfa8a282cd6de50
hemesath <jonas-hemesath@web.de>
2025-05-06 18:40:06 +0200

    ..

--- denoising/create_32bit_stitch_settings.py
+++ denoising/create_32bit_stitch_settings.py
@@ -9,8 +9,16 @@ for folder in os.listdir(target_folder):
     if os.path.isdir(os.path.join(target_folder, folder)):
         with open(os.path.join(target_folder, folder, 'stitch_settings.txt'), 'r') as f:
             lines = f.readlines()
-        template1 = template + '\n' + lines[-2]
-        template1 = template1 + lines[-1]
+        if '16bit' in lines[-2]:
+            line_2 = lines[-2].replace('_16bit', '')
+        else: 
+            line_2 = lines[-2]
+        if '16bit' in lines[-1]:
+            line_1 = lines[-1].replace('_16bit', '')
+        else: 
+            line_1 = lines[-1]
+        template1 = template + '\n' + line_2
+        template1 = template1 + line_1
 
         with open(os.path.join(target_folder, folder, 'stitch_settings_32bit_pi4_5.txt'), 'w') as f:
             f.write(template1)
