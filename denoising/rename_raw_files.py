18b0053a03be0bab1f33f86e15c725ef85a85cc9
hemesath <jonas-hemesath@web.de>
2025-05-13 09:46:15 +0200

    rename rawfiles

---
+++ denoising/rename_raw_files.py
@@ -0,0 +1,26 @@
+import os
+
+
+
+
+parent_folder = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf13_denoising_GT/'
+
+for folder in os.listdir(parent_folder):
+    if os.path.isdir(parent_folder+folder):
+        skip = False
+        for file in os.listdir(parent_folder+folder):
+            if file.endswith('16bit.raw'):
+                skip = True
+        if skip:
+            continue
+        raw_files = []
+        raw_files_size = []
+        for file in os.listdir(parent_folder+folder):
+            if file.endswith('raw'):
+                raw_files.append(file)
+                raw_files_size.append(os.path.getsize(os.path.join(parent_folder, folder, file)))
+
+        raw_files_sort = sorted(zip(raw_files_size, raw_files))
+
+        print(raw_files_sort)
+        
