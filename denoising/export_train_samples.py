6b1a0342bd2b487c12ae387344ef91472903b56b
hemesath <jonas-hemesath@web.de>
2025-05-23 14:25:55 +0200

    ..

--- denoising/export_train_samples.py
+++ denoising/export_train_samples.py
@@ -44,6 +44,7 @@ positions = {}
 for line in postions_data:
     if line:
         line = line.strip('\n').split(', ')
+        print(line)
         positions[line[0]] = [int(line[1]), int(line[2]), int(line[3]), line[7]] 
 
 for folder in sorted(os.listdir(parent_folder)):
