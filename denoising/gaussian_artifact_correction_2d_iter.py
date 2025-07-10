6452bc87313145fbd67dd905e19ba8ef0a6a5f04
hemesath <jonas-hemesath@web.de>
2025-07-10 16:34:32 +0200

    ..

--- denoising/gaussian_artifact_correction_2d_iter.py
+++ denoising/gaussian_artifact_correction_2d_iter.py
@@ -59,43 +59,46 @@ def correct_cupping_artifact_masked(
     return final_image.astype(np.float32), final_background.astype(np.float32)
 # --- Example Usage ---
 # Assume 'my_z_plane' is your float32 NumPy array with zeroed corners
-parent_folder = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf13_denoising_GT/'
+#parent_folder = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf13_denoising_GT/'
 
-done_folders = []
+#done_folders = []
+
 
 SIGMA_FOR_BLUR = 150
 
-for folder in sorted(os.listdir(parent_folder)):
-    print('\n****************************************************************')
-    print(folder)
-    if os.path.isdir(os.path.join(parent_folder, folder)) and folder != 'train_samples' and folder not in done_folders:
-        raw_files = []
-        raw_files_size = []
-        for file in os.listdir(parent_folder+folder):
-            if file.endswith('raw'):
-                raw_files.append(file)
-                raw_files_size.append(os.path.getsize(os.path.join(parent_folder, folder, file)))
+skip = False
 
-        raw_files_sort = sorted(zip(raw_files_size, raw_files))
+raw_files = []
+raw_files_size = []
+for file in os.listdir():
+    if file.endswith('raw'):
+        if 'gauss_corr_sigma' in file:
+            skip = True
+            break
+        raw_files.append(file)
+        raw_files_size.append(os.path.getsize(os.path.join(file)))
 
-        
-        largest_files = [raw_files_sort[-1][1], raw_files_sort[-2][1]]
+if not skip:
+    raw_files_sort = sorted(zip(raw_files_size, raw_files))
 
-        for file in largest_files:
-            print('Loading vol0')
-            vol0 = pi.read(os.path.join(parent_folder, folder, file))
-            output_img = pi.newlike(vol0)
-            vol0 = vol0.get_data()
-            print(vol0.shape)
-            for i in tqdm(range(vol0.shape[2])):
-                corrected_slice, estimated_background = correct_cupping_artifact_masked(vol0[:,:,i], SIGMA_FOR_BLUR)
-                vol0[:,:,i] = corrected_slice
-            output_img.set_data(vol0)
-            if '_0_' in file:
-                fn = file.split('_0_')[0] + '_0_gauss_corr_sigma' + str(SIGMA_FOR_BLUR) + '_' 
-            else:
-                fn = file.split('_1_')[0] + '_1_gauss_corr_sigma' + str(SIGMA_FOR_BLUR) + '_'
-            pi.writeraw(output_img, fn)
+
+    largest_files = [raw_files_sort[-1][1], raw_files_sort[-2][1]]
+
+    for file in largest_files:
+        #print('Loading vol0')
+        vol0 = pi.read(os.path.join(file))
+        output_img = pi.newlike(vol0)
+        vol0 = vol0.get_data()
+        #print(vol0.shape)
+        for i in tqdm(range(vol0.shape[2])):
+            corrected_slice, estimated_background = correct_cupping_artifact_masked(vol0[:,:,i], SIGMA_FOR_BLUR)
+            vol0[:,:,i] = corrected_slice
+        output_img.set_data(vol0)
+        if '_0_' in file:
+            fn = file.split('_0_')[0] + '_0_gauss_corr_sigma' + str(SIGMA_FOR_BLUR) + '_' 
+        else:
+            fn = file.split('_1_')[0] + '_1_gauss_corr_sigma' + str(SIGMA_FOR_BLUR) + '_'
+        pi.writeraw(output_img, fn)
             
         
 """
---
+++ denoising/gaussian_artifact_correction_2d_parallel.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+parent_folder = '/cajal/scratch/projects/xray/bm05/converted_data/new_Sep_2024/zf13_denoising_GT/'
+
+done_folders = []
+
+active_processes = []
+print('Starting processes')
+for folder in os.listdir(parent_folder):
+    wd = parent_folder + folder
+    if os.path.isdir(os.path.join(parent_folder, folder)) and folder != 'train_samples' and folder not in done_folders:
+        active_processes.append(subprocess.Popen(['srun', '--time=7-0', '--gres=gpu:0', '--mem=900000', '--tasks', '1', '--cpus-per-task', '32', '--pty', 'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/denoising/gaussian_artifact_correction_2d_iter.py'],
+                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd))
+
+for i, p in enumerate(active_processes):
+    msg = p.communicate()
+    if msg[1]:
+        print(msg)
+    print('Process', i+1, 'of', len(active_processes), 'done')
+
+        
+        
