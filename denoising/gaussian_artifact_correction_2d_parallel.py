64b67bd60f58b09459318c59366e4e1e634e0947
hemesath <jonas-hemesath@web.de>
2025-07-10 16:44:41 +0200

    ..

--- denoising/gaussian_artifact_correction_2d_parallel.py
+++ denoising/gaussian_artifact_correction_2d_parallel.py
@@ -8,11 +8,12 @@ done_folders = []
 active_processes = []
 print('Starting processes')
 for folder in os.listdir(parent_folder):
-    print(folder)
-    wd = parent_folder + folder
-    if os.path.isdir(os.path.join(parent_folder, folder)) and folder != 'train_samples' and folder not in done_folders:
-        active_processes.append(subprocess.Popen(['srun', '--time=7-0', '--gres=gpu:0', '--mem=900000', '--tasks', '1', '--cpus-per-task', '32', '--pty', 'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/denoising/gaussian_artifact_correction_2d_iter.py'],
-                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd))
+    if os.path.isdir(parent_folder + folder):
+        print(folder)
+        wd = parent_folder + folder
+        if os.path.isdir(os.path.join(parent_folder, folder)) and folder != 'train_samples' and folder not in done_folders:
+            active_processes.append(subprocess.Popen(['srun', '--time=7-0', '--gres=gpu:0', '--mem=900000', '--tasks', '1', '--cpus-per-task', '32', '--pty', 'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/denoising/gaussian_artifact_correction_2d_iter.py'],
+                                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=wd))
 print('Processes submitted')
 for i, p in enumerate(active_processes):
     msg = p.communicate()
