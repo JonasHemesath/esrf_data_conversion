1a14a8cd5593ab58b0a204a7bda228e56cecda08
hemesath <jonas-hemesath@web.de>
2025-07-29 11:29:28 +0200

    ..

--- denoising/remove_files_pattern.py
+++ denoising/remove_files_pattern.py
@@ -1,13 +1,12 @@
 import os
+import sys
 
-pattern = input('Files with which pattern should be removed?\n')
+pattern = sys.argv[1]
 
-confirm = input('Are you sure you want to remove all files with this pattern in the subdirectories of the current location?\n\n' + pattern + '\n\ny/n\n')
 
-if confirm == 'y':
 
-    for folder in os.listdir():
-        if os.path.isdir(folder):
-            for file in os.listdir(folder):
-                if pattern in file:
-                    os.remove(os.path.join(folder, file))
+for folder in os.listdir():
+    if os.path.isdir(folder):
+        for file in os.listdir(folder):
+            if pattern in file:
+                os.remove(os.path.join(folder, file))
