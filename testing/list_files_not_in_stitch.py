8c3346e71822ce21aacaf3b5416b3ea9bbf60228
hemesath <jonas-hemesath@web.de>
2025-08-07 17:21:14 +0200

    testings

---
+++ testing/list_files_not_in_stitch.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+folder = sys.argv[1]
+
+files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+
+txt_file = sys.argv[2]
+filters = []
+if len(sys.argv) > 3:
+    filters = [f for f in sys.argv[3:]]
+
+with open(txt_file, 'r') as f:
+    txt = f.readlines()
+
+
+for line in txt:
+    if line != '\n' and line != '':
+        if filters:
+            if any([filt in line for filt in filters]):
+                continue
+        pot_f = line.split(' = ')[0]
+        if pot_f not in files:
+            print('This file is not in the folder:', pot_f)
+            pot_f2 = pot_f.split('__1_')[0]
+
+            for file in files:
+                if pot_f2 in file:
+                    print('But this might have replaced it:', file)
+                    break
+            print('\n')
+
+
+print('all done')
