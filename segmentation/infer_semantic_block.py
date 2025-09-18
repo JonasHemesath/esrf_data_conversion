616903978d971b6c01a50cbda77fb70debd4b371
hemesath <jonas-hemesath@web.de>
2025-09-18 10:27:42 +0200

    ..

--- segmentation/infer_semantic_block.py
+++ segmentation/infer_semantic_block.py
@@ -10,9 +10,9 @@ pi = Pi2()
 
 file = sys.argv[1]
 
-img = pi.readrawblock('img', file, 0, 0, 0, 500, 500, 500, ImageDataType.UINT16)
+img = pi.readrawblock('img', file, 0, 0, 0, 500, 500, 500, ImageDataType.UINT16).to_numpy()
 
-img_np = img.get_data()
+#img_np = img.get_data()
 
 
-tifffile.imwrite('test.tiff', img_np)
+tifffile.imwrite('test.tiff', img)
