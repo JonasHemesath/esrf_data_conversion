7f2227ef5a0bb697168d5c5d01c818bbcf0a81fe
hemesath <jonas-hemesath@web.de>
2025-11-07 11:47:52 +0100

    setup sematic segmentation whole data

--- segmentation/infer_semantic_block.py
+++ segmentation/infer_semantic_block.py
@@ -1,12 +1,13 @@
-import numpy as np
-import tifffile
-import tensorstore as ts
-import sys
 import os
+import sys
+
+import argparse
+
+import numpy as np
+
 import torch
 import torch.nn.functional as F
-from monai.data import ImageReader
-from monai.networks.nets import SegResNet
+from monai.networks.nets import UNet
 from monai.transforms import (
     Compose,
     EnsureChannelFirstd,
@@ -20,94 +21,72 @@ from pi2py2 import *
 pi = Pi2()
 
 
+parser = argparse.ArgumentParser(description="3D Brain Tissue Segmentation")
+parser.add_argument('--data_path', type=str, required=True, 
+                        help='Path to the dataset')
+parser.add_argument('--dataset_shape', nargs=3, type=int, required=True, 
+                        help='Shape of the dataset')
+parser.add_argument('--dataset_dtype', type=str, choices=['uint8', 'uint16'], required=True, 
+                        help='Datatype of the dataset')
+parser.add_argument('--block_orgin', nargs=3, type=int, required=True, 
+                        help='Origin of the block to load')
+parser.add_argument('--block_shape', nargs=3, type=int, required=True, 
+                        help='Shape of the block to load')
+parser.add_argument('--output_dtype', type=str, choices=['uint32', 'uint64'], required=True, 
+                        help='Datatype of the output')
+parser.add_argument('--output_name', type=str, required=True, 
+                        help='Filename of the output')
+parser.add_argument('--model_path', type=str, required=True, 
+                        help='Path to the model')
 
-si = 0
+args = parser.parse_args()
 
-dataset_name = sys.argv[1]
+if args.dataset_dtype == 'uint8':
+    dataset_dtype = np.uint8
+elif args.dataset_dtype == 'uint16':
+    dataset_dtype = np.uint16
 
-block_org = int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])
-block_size = int(sys.argv[5])
-model_path = sys.argv[6]
+if args.output_dtype == 'uint32':
+    output_dtype = ImageDataType.UINT32
+elif args.output_dtype == 'uint64':
+    output_dtype = ImageDataType.UINT64
 
-iden = 'https://syconn.esc.mpcdf.mpg.de/johem/ng/' + dataset_name + '/'
-#iden = '/cajal/nvmescratch/projects/from_ssdscratch/songbird/johem/ng/' + dataset_name + '/'
 
-dataset_future = ts.open({
-     'driver':
-        'neuroglancer_precomputed',
-    'kvstore':
-         #'https://syconn.esc.mpcdf.mpg.de/johem/ng/zf13_hr2/',
-        iden,
-    'scale_index':
-        si,
-     # Use 100MB in-memory cache.
-     'context': {
-         'cache_pool': {
-             'total_bytes_limit': 100_000_000
-         }
-     },
-     'recheck_cached_data':
-         'open',
-})
+data = np.memmap(args.data_path, dtype=dataset_dtype, mode='r', shape=tuple(args.dataset_shape), order='F')
+vol = data[args.block_orgin[0]:args.block_orgin[0]+args.block_shape[0],
+           args.block_orgin[1]:args.block_orgin[1]+args.block_shape[1],
+           args.block_orgin[2]:args.block_orgin[2]+args.block_shape[2]]
 
-dataset = dataset_future.result()
 
-#print(dataset)
-
-dataset_3d = dataset[ts.d['channel'][0]]
-
-out_shape = [dataset_3d.shape[2], dataset_3d.shape[1], dataset_3d.shape[0]]
-#print(dataset_3d.shape)
-#print(dataset_3d.dtype)
-
-if str(dataset_3d.dtype) == 'dtype("uint8")':
-    data_type = np.uint8
-elif str(dataset_3d.dtype) == 'dtype("uint16")':
-    data_type = np.uint16
-
-block_size_x = min(block_size, dataset_3d.shape[0]-block_org[0])
-block_size_y = min(block_size, dataset_3d.shape[1]-block_org[1])
-block_size_z = min(block_size, dataset_3d.shape[2]-block_org[2])
-
-vol = dataset_3d[
-                block_org[0]:block_org[0]+block_size_x,
-                block_org[1]:block_org[1]+block_size_y,
-                block_org[2]:block_org[2]+block_size_z
-                ].read().result()
-
-vol = vol.transpose(2,1,0)
-#print(type(vol))
-
-if np.sum(vol) > 0 and block_size_x > 100 and block_size_y > 100 and block_size_z > 100:
+if np.sum(vol) > 0 and args.block_shape[0] > 100 and args.block_shape[1] > 100 and args.block_shape[2] > 100:
 
     # --- Device Configuration ---
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}")
+    #print(f"Using device: {device}")
 
     # --- Model Definition ---
     # This is a SegResNet, configured to match the pretrained model architecture.
     # The out_channels is set to 4 for your specific task (BG, vessels, myelin, somata).
-    model = SegResNet(
+    model = UNet(
         spatial_dims=3,
         in_channels=1,
         out_channels=4,
-        init_filters=32,
-        blocks_down=[1, 2, 2, 4],
-        blocks_up=[1, 1, 1],
-        dropout_prob=0.2,
+        channels=(16, 32, 64, 128, 256),
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
     ).to(device)
 
 
 
-    print("--- Starting Prediction ---")
-    if not model_path or not os.path.exists(model_path):
+    #print("--- Starting Prediction ---")
+    if not args.model_path or not os.path.exists(args.model_path):
         raise ValueError("Model path must be provided and exist for prediction.")
 
-    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.load_state_dict(torch.load(args.model_path, map_location=device))
     model.eval()
 
     # --- Prediction ---
-    a_max_val = 255.0 if data_type == np.uint8 else 65535.0
+    a_max_val = 255.0 if dataset_dtype == np.uint8 else 65535.0
 
     pred_transforms = Compose(
         [
@@ -136,11 +115,11 @@ if np.sum(vol) > 0 and block_size_x > 100 and block_size_y > 100 and block_size_
 
 
 
-    img_pi = pi.newimage(ImageDataType.UINT32, block_size, block_size, block_size)
-    img_pi.from_numpy(pred_output_np[50:block_size_z-50, 50:block_size_y-50, 50:block_size_x-50])
+    img_pi = pi.newimage(output_dtype, args.block_shape[0], args.block_shape[1], args.block_shape[2])
+    img_pi.from_numpy(pred_output_np[50:args.block_shape[0]-50, 50:args.block_shape[1]-50, 50:args.block_shape[2]-50])
 
-    out_name = dataset_name + '_semantic_seg_' + str(out_shape[0]) + 'x' + str(out_shape[1]) + 'x' + str(out_shape[2]) + '.raw'
+    #out_name = dataset_name + '_semantic_seg_' + str(out_shape[0]) + 'x' + str(out_shape[1]) + 'x' + str(out_shape[2]) + '.raw'
 
 
-    pi.writerawblock(img_pi, out_name, [block_org[2]+50, block_org[1]+50, block_org[0]+50], [0, 0, 0], [0, 0, 0], [block_size_z-100, block_size_y-100, block_size_x-100])
-    print('done')
+    pi.writerawblock(img_pi, args.output_name, [args.block_origin[0]+50, args.block_origin[1]+50, args.block_origin[2]+50], [0, 0, 0], [0, 0, 0], [args.block_shape-100, args.block_shape-100, args.block_shape-100])
+    #print('done')
--- segmentation/infer_whole_datasset_semantic.py
+++ segmentation/infer_whole_datasset_semantic.py
@@ -1,64 +1,65 @@
-import sys
-import tensorstore
+import math
+
+import argparse
 
 import subprocess
-import time
 
-si = 0
+parser = argparse.ArgumentParser(description="3D Brain Tissue Segmentation")
+parser.add_argument('--data_path', type=str, required=True, 
+                        help='Path to the dataset')
+parser.add_argument('--dataset_shape', nargs=3, type=int, required=True, 
+                        help='Shape of the dataset')
+parser.add_argument('--dataset_dtype', type=str, choices=['uint8', 'uint16'], required=True, 
+                        help='Datatype of the dataset')
+parser.add_argument('--block_shape', nargs=3, type=int, required=True, 
+                        help='Shape of the block to load, should be larger than 200')
+parser.add_argument('--output_dtype', type=str, choices=['uint32', 'uint64'], required=True, 
+                        help='Datatype of the output')
+parser.add_argument('--output_name', type=str, required=True, 
+                        help='Descriptive name of the output')
+parser.add_argument('--model_path', type=str, required=True, 
+                        help='Path to the model')
+args = parser.parse_args()
 
-dataset_name = sys.argv[1]
-
-out_wd = sys.argv[2]
+stride = [s-100 for s in args.block_shape]
 
 
-block_size = int(sys.argv[3])
-step_size = block_size - 100
-model_path = sys.argv[4]
+out_name = args.output_name + '_semantic_seg_' + str(args.dataset_shape[0]) + 'x' + str(args.dataset_shape[1]) + 'x' + str(args.dataset_shape[2]) + '.raw'
 
-iden = 'https://syconn.esc.mpcdf.mpg.de/johem/ng/' + dataset_name + '/'
-#iden = '/cajal/nvmescratch/projects/from_ssdscratch/songbird/johem/ng/' + dataset_name + '/'
 
-dataset_future = ts.open({
-     'driver':
-        'neuroglancer_precomputed',
-    'kvstore':
-         #'https://syconn.esc.mpcdf.mpg.de/johem/ng/zf13_hr2/',
-        iden,
-    'scale_index':
-        si,
-     # Use 100MB in-memory cache.
-     'context': {
-         'cache_pool': {
-             'total_bytes_limit': 100_000_000
-         }
-     },
-     'recheck_cached_data':
-         'open',
-})
-
-dataset = dataset_future.result()
-
-#print(dataset)
-
-dataset_3d = dataset[ts.d['channel'][0]]
-
-dataset_shape = dataset_3d.shape
+x_chunks = math.ceil(args.dataset_shape[0]/stride[0])
+y_chunks = math.ceil(args.dataset_shape[1]/stride[1])
+z_chunks = math.ceil(args.dataset_shape[2]/stride[2])
 
 processes = []
 
-for x in range(0, dataset_shape[0], step_size):
-    for y in range(0, dataset_shape[1], step_size):
-        for z in range(0, dataset_shape[2], step_size):
+for x in range(x_chunks):
+    x_org = x * stride[0]
+    block_x = min(args.block_shape[0], args.dataset_shape[0]-x_org)
 
-            processes.append(subprocess.Popen(['srun', '--time=7-0', '--gres=gpu:a40:1', '--mem=400000', '--tasks', '1', '--cpus-per-task', '32', '--pty', 
-                                    'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/segmentation/infer_semantic_block.py', 
-                                    dataset_name, str(x), str(y), str(z), str(block_size), model_path],
-                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=out_wd))
-            time.sleep(2)
+    for y in range(y_chunks):
+        y_org = y * stride[1]
+        block_y = min(args.block_shape[1], args.dataset_shape[1]-y_org)
 
+        for z in range(z_chunks):
+            z_org = z * stride[2]
+            block_z = min(args.block_shape[2], args.dataset_shape[2]-z_org)
+
+            processes.append(subprocess.Popen(['srun', '--time=7-0', '--gpu:a40:1', '--mem=400000', '--tasks', '1', '--cpus-per-task', '32', '--pty', 'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/segmentation/infer_semantic_block.py',
+                                               '--data_path', args.data_path,
+                                               '--dataset_shape', str(args.dataset_shape[0]), str(args.dataset_shape[1]), str(args.dataset_shape[2]),
+                                               '--dataset_dtype', args.dataset_dtype,
+                                               '--block_orgin', str(x_org), str(y_org), str(z_org),
+                                               '--block_shape', str(block_x), str(block_y), str(block_z),
+                                               '--output_dtype', args.output_dtype,
+                                               '--output_name', out_name,
+                                               '--model_path', args.model_path],
+                                            stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+            
 
 for i, process in enumerate(processes):
-    output = process.communicate()
-    if output[1]:
-        print(output)
-    print('Process', i+1, 'of', len(processes), 'finished')
+    outs, errs = process.communicate()
+    if errs:
+        print(errs)
+    else:
+        print('Process', i+1, 'of', len(processes), 'done')
