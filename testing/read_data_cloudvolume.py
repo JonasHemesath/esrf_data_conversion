0bdb8b5ca13c802b11537cd5e3aae4d23378b6d5
hemesath <jonas-hemesath@web.de>
2026-01-07 09:23:41 +0100

    test_cloudvolume read

---
+++ testing/read_data_cloudvolume.py
@@ -0,0 +1,11 @@
+from cloudvolume import CloudVolume
+
+
+import tifffile
+
+path = '/cajal/scratch/projects/xray/bm05/ng/zf13_hr2_v251209'
+
+image = CloudVolume(path, mip=5, progress=True)
+
+data = image[:,:,:]
+tifffile.imwrite('zf13_mip5.tif', data, imagej=True)
