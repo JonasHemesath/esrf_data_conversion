c55ff6183bdab69f6c6d1e6cba9e259a5562f70d
hemesath <jonas-hemesath@web.de>
2026-01-15 14:50:44 +0100

    ..

--- testing/read_data_cloudvolume.py
+++ testing/read_data_cloudvolume.py
@@ -2,13 +2,16 @@ from cloudvolume import CloudVolume
 
 
 import tifffile
+import sys
 
+cube_size = int(sys.argv[1])
+pos = 4000
 path = '/cajal/scratch/projects/xray/bm05/ng/zf13_hr2_v251209'
 
 image = CloudVolume(path, mip=0, progress=True)
 print([int(i) for i in image.shape])
 print(image.info['scales'][5]['size'])
 
-data = image[4000:4100,4000:4100,4000:4100]
+data = image[pos:pos+cube_size,pos:pos+cube_size,pos:pos+cube_size]
 print(data.shape)
 #tifffile.imwrite('zf13_mip5.tif', data, imagej=True)
