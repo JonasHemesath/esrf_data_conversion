63c30632e5d559ef1ea0051f4ee05fcbf2cf871c
hemesath <jonas-hemesath@web.de>
2026-01-28 09:40:29 +0100

    more mem

--- ij_read_dering_convert_export_mp.py
+++ ij_read_dering_convert_export_mp.py
@@ -15,7 +15,7 @@ args = parser.parse_args()
 
 processes = []
 for i in range(args.processes):
-    processes.append(subprocess.Popen(['srun', '--time=7-0', '--gres=gpu:0', '--mem=400000', '--tasks', '1', '--cpus-per-task', '32', 'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/ij_read_dering_convert_export.py',
+    processes.append(subprocess.Popen(['srun', '--time=7-0', '--gres=gpu:0', '--mem=900000', '--tasks', '1', '--cpus-per-task', '32', 'python', '/cajal/nvmescratch/users/johem/esrf_data_conversion/ij_read_dering_convert_export.py',
                        args.sample, str(i), str(args.processes)],
                       stdout=subprocess.PIPE, stderr=subprocess.PIPE))
     
