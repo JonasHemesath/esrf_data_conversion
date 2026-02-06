23f381e4a41d8b3cd724211d1bf2e6a0f60ece3d
hemesath <jonas-hemesath@web.de>
2026-02-06 17:47:58 +0100

    compile overviews

---
+++ overviews/read_and_save_overviews.py
@@ -0,0 +1,36 @@
+import os
+import tifffile
+
+import numpy as np
+import json
+
+
+
+
+
+paths = [
+    '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/zf13_overview_start/zf13_overview_start_good/zf13_overview_start_good_1_1_0000pag_db0100_vol/zf13_overview_start_good_1_1_0000pag_db0100_vol',
+    '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/zf13_overview_start/zf13_overview_start_0002/zf13_overview_start_0002_1_1_0000pag_db0100_vol/zf13_overview_start_0002_1_1_0000pag_db0100_vol',
+    '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/zf13_overview_start/zf13_overview_start_0003/zf13_overview_start_0003_1_1_0000pag_db0100_vol/zf13_overview_start_0003_1_1_0000pag_db0100_vol'
+]
+
+save_path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Aug_2025/zf13_overview/32bit'
+
+low_percentiles = []
+high_percentiles = []
+percentile_samples = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 999]
+
+for i, path in enumerate(paths):
+    im_list = [tifffile.imread(path + '/' + f) for f in sorted(os.listdir(path)) if f[-4:] == 'tiff']
+    im_stack = np.stack(im_list, axis=0)
+    for p in percentile_samples:
+        low_p = np.percentile(im_stack[p,:,:], 0.39)
+        high_p = np.percentile(im_stack[p,:,:], 99.61)
+        low_percentiles.append(low_p)
+        high_percentiles.append(high_p)
+    with open(os.path.join(save_path, 'percentiles.json'), 'w') as f:
+        json.dump({'low_percentiles': low_percentiles, 'high_percentiles': high_percentiles}, f)
+    tifffile.imwrite(os.path.join(save_path, str(i) + '.tiff'), im_stack, imagej=True)
+
+print('Low percentile', np.mean(low_percentiles))
+print('High percentile', np.mean(high_percentiles))
