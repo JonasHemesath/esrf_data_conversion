78d87c19428c01b731c0d4b115095804b2ee83a7
hemesath <jonas-hemesath@web.de>
2026-02-10 10:48:25 +0100

    ..

--- overviews/test_read_vol.py
+++ overviews/test_read_vol.py
@@ -1,7 +1,13 @@
+import sys
 import tifffile
 
 
 path = '/cajal/scratch/projects/xray/bm05/converted_data/new_Aug_2025/zf13_overview/32bit_ij/0.tif'
-
+with tifffile.TiffFile(path) as tif:
+    if not tif.pages:
+        print(f"Error: TIFF file {path} contains no pages.")
+        sys.exit(1)
+    first_page = tif.pages[0]
+    print(first_page.shape)
 im = tifffile.imread(path, key=1)
 print('Image shape:', im.shape)
