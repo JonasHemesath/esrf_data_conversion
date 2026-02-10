834897231054f1c7854d442203ecad8ce45c84a6
hemesath <jonas-hemesath@web.de>
2026-02-10 10:49:49 +0100

    ..

--- overviews/test_read_vol.py
+++ overviews/test_read_vol.py
@@ -7,7 +7,9 @@ with tifffile.TiffFile(path) as tif:
     if not tif.pages:
         print(f"Error: TIFF file {path} contains no pages.")
         sys.exit(1)
-    first_page = tif.pages[0]
+    first_page = tif.pages[1]
+    print(first_page)
+    print(type(first_page))
     print(first_page.shape)
 im = tifffile.imread(path, key=1)
 print('Image shape:', im.shape)
