fe08820f4308685bea1c704ad3d74b004c81353f
hemesath <jonas-hemesath@web.de>
2026-02-10 11:16:58 +0100

    ..

--- ij_read_dering_convert_export_single_mp.py
+++ ij_read_dering_convert_export_single_mp.py
@@ -254,7 +254,7 @@ if __name__ == '__main__':
             preloaded_im = tifffile.imread(file)
             with multiprocessing.Pool(processes=num_processes) as pool:
                 # Use imap_unordered for memory-efficient processing of results
-                results_iterator = pool.imap_unordered(worker_func, [(i, preloaded_im[i,:,:]) for i in range(im_shape[0])])
+                results_iterator = pool.imap_unordered(worker_func, [(i, preloaded_im[i,:,:]) for i in range(z)])
                 
                 # Process results as they complete and show progress
                 for i, processed_slice in tqdm(results_iterator, total=z):
