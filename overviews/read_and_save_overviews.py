c46516e6362b02f47aec6ec386c22078d37e83ed
hemesath <jonas-hemesath@web.de>
2026-02-10 09:34:47 +0100

    fix overview save

--- overviews/read_and_save_overviews.py
+++ overviews/read_and_save_overviews.py
@@ -1,12 +1,15 @@
 import os
 import tifffile
-
+import imagej
+import scyjava
 import numpy as np
 import json
 
 
 
-
+scyjava.config.add_option('-Xmx500g')
+ij = imagej.init()
+print('ij loaded')
 
 paths = [
     '/cajal/scratch/projects/xray/bm05/20230913/PROCESSED_DATA/zf13_overview_start/zf13_overview_start_good/zf13_overview_start_good_1_1_0000pag_db0100_vol/zf13_overview_start_good_1_1_0000pag_db0100_vol',
@@ -23,6 +26,8 @@ percentile_samples = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 999]
 for i, path in enumerate(paths):
     im_list = [tifffile.imread(path + '/' + f) for f in sorted(os.listdir(path)) if f[-4:] == 'tiff']
     im_stack = np.stack(im_list, axis=0)
+    del im_list
+    print('Image shape:', im_stack.shape)
     for p in percentile_samples:
         low_p = np.percentile(im_stack[p,:,:], 0.39)
         high_p = np.percentile(im_stack[p,:,:], 99.61)
@@ -30,7 +35,14 @@ for i, path in enumerate(paths):
         high_percentiles.append(high_p)
     with open(os.path.join(save_path, 'percentiles.json'), 'w') as f:
         json.dump({'low_percentiles': low_percentiles, 'high_percentiles': high_percentiles}, f)
-    tifffile.imwrite(os.path.join(save_path, str(i) + '.tiff'), im_stack, imagej=True)
+    #tifffile.imwrite(os.path.join(save_path, str(i) + '.tiff'), im_stack, imagej=True)
+    im_ij = ij.py.to_dataset(im_stack, dim_order=['pln', 'row', 'col'])
+    print('ij conversion done')
+    del im_stack
+
+    ij.io().save(im_ij, os.path.join(save_path, str(i) + '.tiff'))
+    print('ImageJ: image saved')
+    del im_ij
 
 print('Low percentile', np.mean(low_percentiles))
 print('High percentile', np.mean(high_percentiles))
