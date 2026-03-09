451d5c0453551d901b95fef233cbaa5164190326
hemesath <jonas-hemesath@web.de>
2026-03-09 15:27:18 +0100

    ..

--- analysis/test/get_meshes.py
+++ analysis/test/get_meshes.py
@@ -1,8 +1,8 @@
 from cloudvolume import CloudVolume
 
-vol = CloudVolume('/cajal/scratch/projects/xray/bm05/ng/zf13_hr2_brain_mask_v260305')
+vol = CloudVolume('/cajal/scratch/projects/xray/bm05/ng/zf13_hr2_brain_regions_v260305')
 
-mesh = vol.mesh.get(0)
+mesh = vol.mesh.get(3)
 
 print(mesh)
 print(type(mesh))
