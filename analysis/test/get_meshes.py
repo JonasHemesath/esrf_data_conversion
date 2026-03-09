989f4a95294a2f7fcbd324766e58d49020d82c52
hemesath <jonas-hemesath@web.de>
2026-03-09 15:34:36 +0100

    ..

--- analysis/test/get_meshes.py
+++ analysis/test/get_meshes.py
@@ -1,4 +1,5 @@
 from cloudvolume import CloudVolume
+import trimesh
 
 vol = CloudVolume('/cajal/scratch/projects/xray/bm05/ng/zf13_hr2_brain_regions_v260305')
 
@@ -6,3 +7,7 @@ mesh = vol.mesh.get(3)
 
 print(mesh)
 print(type(mesh))
+
+mesh_t = mesh.trimesh()
+print(mesh_t)
+print(type(mesh_t))
