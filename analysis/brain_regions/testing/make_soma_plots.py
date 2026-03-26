e40c04df669accffae7ab06094d40e03d53f57bd
hemesath <jonas-hemesath@web.de>
2026-03-26 12:20:11 +0100

    testing soma

---
+++ analysis/brain_regions/testing/make_soma_plots.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Make soma plots")
+    parser.add_argument("--soma_data_file", type=str, help="Path to the soma data file (numpy array)")
+    args = parser.parse_args()
+
+    brain_region_labels = {
+        1: 'Area X r',
+        3: 'Area X l',
+        4: 'RA r',
+        5: 'RA l',
+        6: 'HVC r',
+        9: 'HVC l',
+        10: 'LMAN r',
+        11: 'LMAN l',
+        12: 'DLM r',
+        13: 'DLM l',
+        14: 'VTA r',
+        15: 'VTA l',
+        16: 'Uva r',
+        17: 'Uva l'
+    }
+
+    soma_data = np.load(args.soma_data_file)
+
+    for label in brain_region_labels.keys():
+        print(f"{label}: {brain_region_labels[label]}")
+        idxs = np.where(soma_data[:, 1] == label and soma_data[:, 2] > 0 and soma_data[:, 3] > 0 and soma_data[:, 4] > 0)[0]
+        surface_areas = soma_data[idxs, 2].flatten()
+        volumes = soma_data[idxs, 3].flatten()
+        convex_hull_volumes = soma_data[idxs, 4].flatten()
+
+        plt.figure(figsize=(15, 5))
+        plt.subplot(1, 3, 1)
+        plt.hist(surface_areas, bins=20)
+        plt.title(f"{brain_region_labels[label]} - Soma Surface Area")
+        plt.xlabel("Surface Area")
+        plt.ylabel("Frequency")
+        plt.subplot(1, 3, 2)
+
+        plt.hist(volumes, bins=20)
+        plt.title(f"{brain_region_labels[label]} - Soma Volume")
+        plt.xlabel("Volume")
+        plt.ylabel("Frequency")
+        plt.subplot(1, 3, 3)
+        plt.hist(convex_hull_volumes, bins=20)
+        plt.title(f"{brain_region_labels[label]} - Soma Convex Hull Volume")
+        plt.xlabel("Convex Hull Volume")
+        plt.ylabel("Frequency")
+        plt.tight_layout()
+        plt.savefig(f"soma_plots_{brain_region_labels[label]}.png")
+        plt.close()
