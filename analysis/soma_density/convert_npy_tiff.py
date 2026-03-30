c7e4e7e46e711799ae293be11d54ff3b9320ce1b
hemesath <jonas-hemesath@web.de>
2026-03-30 14:13:36 +0200

    ..

---
+++ analysis/soma_density/convert_npy_tiff.py
@@ -0,0 +1,14 @@
+import argparse
+import tifffile
+
+import numpy as np
+
+args = argparse.ArgumentParser()
+args.add_argument("--npy_path", type=str, help="Path to the input .npy file")
+
+if __name__ == "__main__":
+    args = args.parse_args()
+    data = np.load(args.npy_path)
+    out_path = args.npy_path.rsplit(".", 1)[0] + ".tiff"
+    print(f"Saving {data.shape} array to {out_path} ...")
+    tifffile.imwrite(out_path, data, imagej=True)
