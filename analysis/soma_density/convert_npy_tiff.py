2efac03287810c6de0efeabd796067e1c687e55a
hemesath <jonas-hemesath@web.de>
2026-03-30 14:16:04 +0200

    ..

--- analysis/soma_density/convert_npy_tiff.py
+++ analysis/soma_density/convert_npy_tiff.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     data = np.load(args.npy_path)
     out_path = args.npy_path.rsplit(".", 1)[0] + ".tiff"
     print(f"Saving {data.shape} array to {out_path} ...")
-    tifffile.imwrite(out_path, data, imagej=True)
+    tifffile.imwrite(out_path, data.astype(np.uint16), imagej=True)
