3416a6c75ed5c93e4a5b37904716ec9b2255efc7
hemesath <jonas-hemesath@web.de>
2026-04-09 16:47:29 +0200

    ..

--- analysis/BV/testing/print_verts.py
+++ analysis/BV/testing/print_verts.py
@@ -5,5 +5,4 @@ if __name__ == "__main__":
     path = sys.argv[1]
     vol = CloudVolume(path)
     skeleton = vol.skeleton.get(1)
-    for vertex in skeleton.vertices:
-        print(vertex)
+    print(len(skeleton.vertices))
