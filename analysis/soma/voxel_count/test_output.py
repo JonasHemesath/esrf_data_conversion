342c0ecacd1569c03187aceb2d1bf2ab5cb2a7f0
hemesath <jonas-hemesath@web.de>
2026-04-16 15:13:57 +0200

    testing

---
+++ analysis/soma/voxel_count/test_output.py
@@ -0,0 +1,15 @@
+from cloudfiles import CloudFile
+from mapbuffer import IntMap
+import sys
+
+cloudpath = sys.argv[1]
+
+
+cf = CloudFile("/".join(cloudpath, "stats", "voxel_counts.im"))
+
+# for (slow) remote access w/o having to download the file
+# only works if file is uncompressed on remote
+#im = IntMap(cf)
+# for fast local access, but downloads the whole file
+im = IntMap(cf.get())
+print(im.shape)
