db733b7dd0945cc329f0233eca5a24d48568bbdf
hemesath <jonas-hemesath@web.de>
2026-04-17 08:56:21 +0200

    testing

---
+++ analysis/brain_regions/prep/test.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+
+base_path = sys.argv[1]
+label = int(sys.argv[2])
+
+
+soma_labels = np.load(base_path + f'_label_{label}.npy')
+soma_index = np.load(base_path + f'_index_{label}.npy')
+
+print(soma_labels)
+input("Press Enter to continue...")
+print(soma_index)
