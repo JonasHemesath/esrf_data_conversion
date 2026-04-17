a4b261a24ab80d4d1c7700b5d9feac9b6e79097f
hemesath <jonas-hemesath@web.de>
2026-04-17 08:58:15 +0200

    ..

--- analysis/brain_regions/prep/test.py
+++ analysis/brain_regions/prep/test.py
@@ -9,5 +9,7 @@ soma_labels = np.load(base_path + f'_label_{label}.npy')
 soma_index = np.load(base_path + f'_index_{label}.npy')
 
 print(soma_labels)
+print(soma_labels.shape)
 input("Press Enter to continue...")
 print(soma_index)
+print(soma_index.shape)
