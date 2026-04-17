b3acc831d0131cfcb1d3424b39a693023e587b34
hemesath <jonas-hemesath@web.de>
2026-04-17 09:16:26 +0200

    ..

--- analysis/brain_regions/prep/test.py
+++ analysis/brain_regions/prep/test.py
@@ -7,9 +7,13 @@ label = int(sys.argv[2])
 
 soma_labels = np.load(base_path + f'_label_{label}.npy')
 soma_index = np.load(base_path + f'_index_{label}.npy')
+soma_coordinates = np.load(base_path + f'_coordinates_{label}.npy')
 
 print(soma_labels)
 print(soma_labels.shape)
 input("Press Enter to continue...")
 print(soma_index)
 print(soma_index.shape)
+input("Press Enter to continue...")
+print(soma_coordinates)
+print(soma_coordinates.shape)
