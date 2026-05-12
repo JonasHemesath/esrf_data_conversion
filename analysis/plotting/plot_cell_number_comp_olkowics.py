7e419e2473aac936df41e137724a39958488a78b
hemesath <jonas-hemesath@web.de>
2026-05-12 14:14:38 +0200

    ..

--- analysis/plotting/plot_cell_number_comp_olkowics.py
+++ analysis/plotting/plot_cell_number_comp_olkowics.py
@@ -34,7 +34,7 @@ def get_data_for_brain_region(brain_areas_data_path, brain_region_labels_path, s
     print("Loaded brain region labels:", brain_region_labels)
     soma_data = np.load(soma_npy_path)
     print("Loaded soma data with shape:", soma_data.shape)
-    print(np.unique(soma_data[:,14]))  # Unique brain region labels in soma data
+    print(np.unique(soma_data[:,3]))  # Unique brain region labels in soma data
     data_per_brain_region = {}
     for k, v in brain_region_labels.items():
         print(f"Processing brain region label: {k}")
@@ -42,9 +42,9 @@ def get_data_for_brain_region(brain_areas_data_path, brain_region_labels_path, s
         brain_region_name = v[0]
         
         
-        soma_data_in_region = soma_data[soma_data[:,14] == brain_region_label]
+        soma_data_in_region = soma_data[soma_data[:,3] == brain_region_label]
         # Filter out somata with non-positive volume
-        soma_data_in_region = soma_data_in_region[soma_data_in_region[:, 4] > 0]
+        soma_data_in_region = soma_data_in_region[soma_data_in_region[:, 5] > 0]
         
         if brain_region_name == "Diencephalon":
             
