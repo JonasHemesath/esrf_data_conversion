c0629eecd6100de67df1995e368acb1d65f0f310
hemesath <jonas-hemesath@web.de>
2026-05-21 09:35:12 +0200

    ..

---
+++ analysis/BV/get_skeleton_for_blender.py
@@ -0,0 +1,31 @@
+
+import os
+from cloudvolume import CloudVolume
+import json
+
+
+
+if __name__ == "__main__":
+    brain_region_labels_path = "/cajal/nvmescratch/users/johem/esrf_data_conversion/analysis/brain_regions/brain_region_labels_v260409.json"
+    BV_path = "/cajal/scratch/projects/xray/bm05/ng/BV_testing/260304_Myelin_BV_multires_multipath_linearLR_BV_masked_brain_regions"
+    output_dir = "/cajal/scratch/projects/xray/bm05/ng/BV_testing/260304_Myelin_BV_multires_multipath_linearLR_BV_masked_brain_regions/analysis_results"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with open(brain_region_labels_path, "r") as f:
+        brain_region_labels = json.load(f)
+    print(f"Loaded {len(brain_region_labels)} brain region labels")
+    
+    bv = CloudVolume(BV_path)
+
+    graph_dict = {"graphs": []}
+
+    for brain_region_label in brain_region_labels:
+        print(f"Processing brain region {brain_region_label}...")
+        label = int(brain_region_label)
+        skeleton = bv.skeleton.get(label)
+        graph_dict["graphs"].append({"name": label, "nodes": [{"id": i, "pos": vertex.tolist(), "radius": skeleton.radii[i]} for i, vertex in enumerate(skeleton.vertices)], "edges": [[edge[0], edge[1]] for edge in skeleton.edges]})
+
+    output_path = f"{output_dir}/skeleton_graphs.json"
+    with open(output_path, "w") as f:
+        json.dump(graph_dict, f) 
